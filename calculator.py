90c2b53444dbb04b6facfe3a364f64e64e2391e0
SAMARTH D <69334426+samarth0337@users.noreply.github.com>
2023-07-23 19:08:49 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+num1= input("enter first number:")
+num2= input("enter 2nd number:")
+
+print(int(num1) + int(num2))
+print(int(num1) - int(num2))
+print(int(num1) * int(num2))
+print(int(num1) / int(num2))
